dac8a707722f733eb9c429d36d1cf2d32a63f080
cvalencia1991 <cesar4a6z@gmail.com>
2022-11-15 16:57:25 -0500

    Add Table Mission

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 export default function Missions() {
+  const missions = useSelector((state) => state.missions);
   return (
-    <div>Hello Missions</div>
+    <table className="table table-bordered table-striped m-5 w-auto p-3">
+      <tbody>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th className="stylesjoin"> </th>
+        </tr>
+        {missions.map((mission) => (
+          <tr key={mission.id}>
+            <th>{mission.mission_name}</th>
+            <th>{mission.description}</th>
+            <th>status</th>
+            <th><Button>Join Mission</Button></th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
