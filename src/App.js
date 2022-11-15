934453adbfea647f79632b163b3b0b4676674935
jfoyarzo <foyarzo621@gmail.com>
2022-11-15 15:35:37 -0300

    Import Provider and store to App.js file and add dispatch of getRockets() on load

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, Provider } from 'react-redux';
 import NavbarSpace from './Components/Navbar';
 import './App.css';
 import Myprofile from './Components/Myprofile';
 import Rockets from './Components/Rockets';
 import Missions from './Components/Missions';
+import store from './Redux/configureStore';
+import { getRockets } from './Redux/Rockets/rocketsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
   return (
-    <Router>
-      <NavbarSpace />
-      <Routes>
-        <Route exact path="/" element={<Myprofile />} />
-        <Route path="/rockets" element={<Rockets />} />
-        <Route exact path="/Missions" element={<Missions />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <NavbarSpace />
+        <Routes>
+          <Route exact path="/" element={<Myprofile />} />
+          <Route path="/rockets" element={<Rockets />} />
+          <Route exact path="/Missions" element={<Missions />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
