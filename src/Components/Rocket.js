d84de2df36ce44aa867551de186871d5ffbe5d5a
jfoyarzo <foyarzo621@gmail.com>
2022-11-15 19:42:27 -0300

    Add card to Rocket component

--- src/Components/Rocket.js
+++ src/Components/Rocket.js
@@ -1,12 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable camelcase */
 import { React } from 'react';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { CardImg } from 'react-bootstrap';
 
 const Rocket = (props) => {
   const {
-    id, rocket_name, description, flickr_images
+    id, rocket_name, description, flickr_images,
   } = props;
 
   return (
-    
-  )
-}
+    <Card style={{ width: '100%' }}>
+      <Row>
+        <Col>
+          <CardImg src={flickr_images[0]} />
+        </Col>
+        <Col>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card&aposs content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default Rocket;
--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -1,10 +1,20 @@
 import { React } from 'react';
 import { useSelector } from 'react-redux';
 
+
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets);
   return (
-    <div>Rockets</div>
+    <div className="rocket-list">
+      { rockets.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          title={rocket.title}
+          description={rocket.description}
+          id={rocket.id}
+        />
+      ))}
+    </div>
   );
 };
 
