28e8719c4ad09f318ebdae24513379958f395922
jfoyarzo <foyarzo621@gmail.com>
2022-11-15 16:47:29 -0300

    Add Rocket component

---
+++ src/Components/Rocket.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import Card from 'react-bootstrap/Card'
+
+const Rocket = (props) => {
+  const {
+    id, rocket_name, description, flickr_images
+  } = props;
+
+  return (
+    
+  )
+}
--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -1,7 +1,11 @@
 import { React } from 'react';
+import { useSelector } from 'react-redux';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
+  return (
+    <div>Rockets</div>
+  );
+};
 
 export default Rockets;
