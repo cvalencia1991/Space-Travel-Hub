91a0d4276faac81b558d95fb750332ca2c6a2350
jfoyarzo <foyarzo621@gmail.com>
2022-11-15 15:24:27 -0300

    Add cretaeAsyncThunk and action creator for fetching data of rockets

--- src/Redux/Rockets/rocketsSlice.js
+++ src/Redux/Rockets/rocketsSlice.js
@@ -1,9 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = [];
+
+export const getRockets = createAsyncThunk(
+  'rockets/getRockets',
+  async () => {
+    const response = await fetch('https://api.spacexdata.com/v3/rockets');
+    const responseJson = await response.json();
+    return responseJson;
+  },
+);
+
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
+  extraReducers: {
+    [getRockets.fulfilled]: (state, action) => {
+      console.log(action.payload);
+      return [...state];
+    },
+  },
 });
 
 export default rocketsSlice.reducer;
