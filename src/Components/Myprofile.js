ad33848b38b60b6d5b7eab8d6cd307cd90c67a53
cvalencia1991 <cesar4a6z@gmail.com>
2022-11-16 16:52:22 -0500

    Add MyProfile Rows and Columns

--- src/Components/Myprofile.js
+++ src/Components/Myprofile.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Myprofile() {
   return (
-    <div>hello world</div>
+    <Container>
+      <Row>
+        <Col>My Missions</Col>
+        <Col>My Rockets</Col>
+      </Row>
+    </Container>
   );
 }
