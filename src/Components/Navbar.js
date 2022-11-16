012e9424cd955a4bb40eb7eebd531a03c96a62da
jfoyarzo <foyarzo621@gmail.com>
2022-11-16 18:32:23 -0300

    Update style of Navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,21 +4,23 @@ import logo from '../imgs/planet.png';
 
 function NavbarSpace() {
   return (
-    <Navbar className="bg-dark">
+    <Navbar className="p-4">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand className="d-flex align-items-center" href="/">
           <img
             src={logo}
-            width="30"
-            height="30"
+            width="80"
+            height="80"
             className="d-inline-block align-top"
             alt="Space Travel Hublogo"
           />
+          <h1 className="d-inline ms-2">Space Traveler&apos;s Hub</h1>
         </Navbar.Brand>
-        <ul className="d-flex p-0 gap-3">
+        <ul className="d-flex p-0 gap-3 navigation" style={{ listStyle: 'none' }}>
           <li>
             <NavLink
               to="/"
+              className={({ isActive }) => (isActive ? 'active-link' : 'link')}
             >
               Rockets
             </NavLink>
@@ -26,13 +28,16 @@ function NavbarSpace() {
           <li>
             <NavLink
               to="Missions"
+              className={({ isActive }) => (isActive ? 'active-link' : 'link')}
             >
               Missions
             </NavLink>
           </li>
+          |
           <li>
             <NavLink
               to="/myProfile"
+              className={({ isActive }) => (isActive ? 'active-link' : 'link')}
             >
               My Profile
             </NavLink>
