9e6c29a73f5b0f0d8b5504efd13d147e821f3980
jfoyarzo <foyarzo621@gmail.com>
2022-11-17 16:29:32 -0300

    Add component rendering tests

--- src/__tests__/NavbarRender.test.js
+++ src/__tests__/NavbarRender.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import Navbar from '../Components/Navbar';
