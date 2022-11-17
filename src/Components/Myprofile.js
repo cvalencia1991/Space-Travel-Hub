44d34592437bf16a6e1f99bf29ed407e63c376ee
jfoyarzo <foyarzo621@gmail.com>
2022-11-16 21:18:39 -0300

    Add Listgroup component for the rocket list to Myprofile view

--- src/Components/Myprofile.js
+++ src/Components/Myprofile.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function Myprofile() {
+  const rockets = useSelector((state) => state.rockets);
+  const reserved = rockets.filter((rocket) => rocket.reserved);
   return (
-    <div>hello world</div>
+    <Container>
+      <Row>
+        <Col>My Missions</Col>
+        <Col>
+          <h2>My Rockets</h2>
+          <ListGroup>
+            { reserved.map((rocket) => (
+              <ListGroup.Item key={rocket.id}>{ rocket.rocket_name }</ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
